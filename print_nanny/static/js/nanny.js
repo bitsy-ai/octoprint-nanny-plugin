9373d49f4e378b478d8a369b831d061316e4d8a1
Leigh Johnson <hi@leighjohnson.me>
2020-11-05 12:50:05 -0800

    first pass on settings view model

--- print_nanny/static/js/nanny.js
+++ print_nanny/static/js/nanny.js
@@ -26,23 +26,149 @@
 //     });
 // });
 
+/*
+** 
+** Helpers
+**
+*/
+
+
+$(function() {
+    function PrintNannySettingsViewModel(parameters) {
+
+    let self = this;
+    self.swaggerClient = undefined;
+    // settings, wizard
+    self.authTokenInput = undefined;
+    self.octoUser = undefined;
+    self.verified = undefined;
+    self.error = undefined;
+
+    // assign the injected parameters, e.g.:
+    self.loginStateViewModel = parameters[0];
+    self.settingsViewModel = parameters[1];
+
+    self.alertClass = ko.observable();
+    self.alerts = {
+        'warning': {
+            header: 'Warning!',
+            text: 'Test connection before saving.',
+            class: 'alert'
+        },
+        'error': {
+            header: 'Error!',
+            text: 'Could not verify token.',
+            class: 'alert-error'
+        },
+        'success': {
+            header: 'Nice!',
+            text: 'Your token is verified',
+            class: 'alert-success'
+        }
+    };
+    self.alertHeader = ko.observable(self.alerts.warning.header)
+    self.alertText = ko.observable(self.alerts.warning.text)
+    self.authTokenInput = ko.observable();
+
+
+    self.basicSettings = [];
+    self.advancedSettings = [];
+
+
+    self.onAllBound = async function(allViewModels){
+        SwaggerClient.http.withCredentials = true;
+        self.swaggerClient = await SwaggerClient({
+            url: self.settingsViewModel.settings.plugins.print_nanny.swagger_json(),
+            authorizations: { BearerAuth: {value: settingsViewModel.settings.plugins.print_nanny.auth_token()} } 
+        })
+        // self.basicSettings = {'auth_token' : {display: 'Token', value: self.settingsViewModel.settings.plugins.print_nanny.auth_token}}
+
+        // self.advancedSettings = [
+        //     {key: 'api_uri', display: 'API URI', value: self.settingsViewModel.settings.plugins.print_nanny.auth_token},
+        //     {key: 'swagger_json', display: 'API URI', value: self.settingsViewModel.settings.plugins.print_nanny.swagger_json},
+        // ]
+    }
+    self.saveSettingsData = function(userData){
+
+        self.notices = {
+            'alertStong': 'Nice!',
+            'alertText': 'Your token is verfied.'
+        }
+        const settings = {
+            plugins: {
+                print_nanny: {
+                    auth_token: self.authTokenInput,
+                    email: userData.email,
+                    lookup: userData.url
+                }
+            }
+        }
+        self.settingsViewModel.saveData(settings);
+    }
+
+    testAuthTokenInput = function(){
+        if (self.authTokenInput == undefined){
+            return
+        }
+        self.swaggerClient.apis.me.get_me()
+            .then((res) =>{
+                self.alertClass(self.alerts.success.class)
+                self.alertHeader(self.alerts.success.header)
+                self.alertText(self.alerts.success.text)
+                self.saveSettingsData(res.body)
+            })
+            .catch(e => {
+                console.error(e)
+                self.alertMode(self.alerts.success.class)
+                self.alertHeader(self.alerts.error.header)
+                self.alertText(self.alerts.error.text)
+            });
+    }
+    
+
+    }
+    OCTOPRINT_VIEWMODELS.push({
+        construct: PrintNannySettingsViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ "loginStateViewModel", "settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_nanny, #tab_plugin_nanny, ...
+        elements: [ '#settings_plugin_print_nanny' ]
+
+    });
+});
+
 
 $(function() {
     function PrintNannyWizardViewModel(parameters) {
-        SwaggerClient.http.withCredentials = true;
 
         let self = this;
         self.swaggerClient = undefined;
         // settings, wizard
-        self.authTokenInput = undefined;
+        self.authTokenInput = ko.observable();
         self.octoUser = undefined;
         self.verified = undefined;
         self.error = undefined;
 
-        self.notices = {
-            'alertStong': 'Warning!',
-            'alertText': 'Test connection before saving.'
-        }
+        // self.alertMode = ko.observable('warning')
+
+        // self.alerts = ko.observable({
+        //     'warning': {
+        //         strong: 'Warning!',
+        //         text: 'Test connection before saving.'
+        //     },
+        //     'error': {
+        //         strong: 'Error!',
+        //         text: 'Could not verify token.'
+        //     },
+        //     'success': {
+        //         strong: 'Nice!',
+        //         text: 'Your token is verified.'
+        //     }
+        // })
+
+        // self.fieldset = ko.observable({
+        //     'auth_token': undefined
+        // })
 
 
         // assign the injected parameters, e.g.:
@@ -75,23 +201,17 @@ $(function() {
             }
             self.settingsViewModel.saveData(settings);
         }
-        testAuthTokenInput = function(){
-            if (self.authTokenInput == undefined){
-                return
-            }
-            SwaggerClient({
-                url: self.settingsViewModel.settings.plugins.print_nanny.swagger_json(),
-                authorizations: { BearerAuth: {value: self.authTokenInput } } 
-            })
-            .then( client => {
-                console.log('Print Nanny swagger-js client initialized', client)
-                self.swaggerClient = client;
-                client.apis.me.get_me()
-                .then(self.saveSettingsData)
-                });
 
-        }
 
+        // TODO: Implement your plugin's view model here.
+
+        // onStartupComplete() = function () {}
+        // onEventPredictDone() = function () {}
+        // onEventCalibrateDone() = function () {}
+        // onEventCalibrateFailed() = function () {}
+
+        
+        // onWizardFinish() = function() {}
     }
 
     /* view model class, parameters for constructor, container to bind to
