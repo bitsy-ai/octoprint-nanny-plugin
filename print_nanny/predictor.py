3acec8f12f2aa8ef8ae6458f9a42dd8ddd873cb3
Leigh Johnson <leigh@bitsy.ai>
2020-12-10 06:13:39 +0000

    first pass on PredictWorker proc

--- print_nanny/predictor.py
+++ print_nanny/predictor.py
@@ -1,4 +1,5 @@
 import base64
+import datetime
 import io
 import threading
 import json
@@ -8,7 +9,9 @@ import os
 from PIL import Image as PImage
 import tensorflow as tf
 import requests
-import pytz
+import queue
+from time import sleep
+
 
 from print_nanny.utils.visualization import visualize_boxes_and_labels_on_image_array
 
@@ -21,8 +24,10 @@ except:
     from typing_extensions import TypedDict
     from typing import Optional
 
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger("octoprint.plugins.print_nanny")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s  - %(name)s - %(levelname)s - %(message)s [%(process)d - %(threadName)s]")
+handler.setFormatter(formatter)
 
 class Prediction(TypedDict):
     num_detections: int
@@ -47,6 +52,7 @@ class ThreadLocalPredictor(threading.local):
         min_score_thresh=0.50,
         max_boxes_to_draw=10,
         min_overlap_area=0.66,
+        calibration = None,
         model_version="tflite-print3d-2020-10-23T18:00:41.136Z",
         model_filename="model.tflite",
         metadata_filename="tflite_metadata.json",
@@ -86,6 +92,8 @@ class ThreadLocalPredictor(threading.local):
             print(self.category_index)
         self.input_shape = self.metadata["inputShape"]
 
+        self.calibration = self.calibration
+
     def load_url_buffer(self, url: str):
         res = requests.get(url)
         res.raise_for_status()
@@ -124,7 +132,8 @@ class ThreadLocalPredictor(threading.local):
             # assert bb1[0] < bb1[2]
             # assert bb1[1] < bb1[3]
             # assert bb2[0] < bb2[2]
-            # assert bb2[1] < bb2[3]
+            # assert bfrom datetime import datetime
+b2[1] < bb2[3]
 
             # determine the coordinates of the intersection rectangle
             x_left = max(bb1[0], bb2[0])
@@ -151,34 +160,47 @@ class ThreadLocalPredictor(threading.local):
 
         return aou
 
-    def postprocess(self, image: PImage, prediction: Prediction, calibration_fn):
+    def postprocess(self, image: PImage, prediction: Prediction):
 
         image_np = np.asarray(image).copy()
         height, width, _ = image_np.shape
-        calibration = calibration_fn(int(prediction["num_detections"]), height, width)
-        detection_boundary_mask = calibration["mask"]
-        coords = calibration["coords"]
-        percent_intersection = self.percent_intersection(
-            prediction["detection_boxes"],
-            prediction["detection_scores"],
-            prediction["detection_classes"],
-            coords,
-        )
-        ignored_mask = percent_intersection <= self.min_overlap_area
 
-        viz = visualize_boxes_and_labels_on_image_array(
-            image_np,
-            prediction["detection_boxes"],
-            prediction["detection_classes"],
-            prediction["detection_scores"],
-            self.category_index,
-            use_normalized_coordinates=True,
-            line_thickness=4,
-            min_score_thresh=self.min_score_thresh,
-            max_boxes_to_draw=self.max_boxes_to_draw,
-            detection_boundary_mask=detection_boundary_mask,
-            detection_box_ignored=ignored_mask,
-        )
+        if self.calibration is not None:
+            detection_boundary_mask = calibration["mask"]
+            coords = calibration["coords"]
+            percent_intersection = self.percent_intersection(
+                prediction["detection_boxes"],
+                prediction["detection_scores"],
+                prediction["detection_classes"],
+                coords,
+            )
+            ignored_mask = percent_intersection <= self.min_overlap_area
+
+            viz = visualize_boxes_and_labels_on_image_array(
+                image_np,
+                prediction["detection_boxes"],
+                prediction["detection_classes"],
+                prediction["detection_scores"],
+                self.category_index,
+                use_normalized_coordinates=True,
+                line_thickness=4,
+                min_score_thresh=self.min_score_thresh,
+                max_boxes_to_draw=self.max_boxes_to_draw,
+                detection_boundary_mask=detection_boundary_mask,
+                detection_box_ignored=ignored_mask,
+            )
+        else:
+            viz = visualize_boxes_and_labels_on_image_array(
+                image_np,
+                prediction["detection_boxes"],
+                prediction["detection_classes"],
+                prediction["detection_scores"],
+                self.category_index,
+                use_normalized_coordinates=True,
+                line_thickness=4,
+                min_score_thresh=self.min_score_thresh,
+                max_boxes_to_draw=self.max_boxes_to_draw,
+            )   
         return viz
 
     def predict(self, image: PImage) -> Prediction:
@@ -208,3 +230,75 @@ class ThreadLocalPredictor(threading.local):
             detection_scores=score_data,
             num_detections=num_detections,
         )
+
+
+class PredictWorker:
+
+    PREDICT_DONE = "predict_done"
+
+    def __init__(self, webcam_url, web_queue, websocket_queue, calibration, fps=5):
+
+        self.calibration = calibration
+        self._fps = fps
+        self._webcam_url = webcam_url
+        self._task_queue = queue.Queue(maxsize=20)
+        
+        self._web_queue = web_queue
+        self._websocket_queue = websocket_queue
+
+        self._predictor = ThreadLocalPredictor(calibration=calibration)
+
+        self._producer_thread = threading.Thread(target=self._producer, daemon=True, name='producer')
+        self._producer_thread.start()
+
+        self._consumer_thread = threading.Thread(target=self._consumer, daemon=True, name='consumer')
+        self._consumer_thread.start()
+
+    def _producer(self):
+        '''
+            Samples frame buffer from webcam stream
+        '''
+        logger.info('Started PredictWorker.producer thread')
+        while True:
+            try:
+                now = datetime.now()
+                self._task_queue.put_nowait({
+                'ts': now
+                'image_buffer': self._predictor.load_url_buffer(self._webcam_url)
+                })
+            except Queue.Full:
+                logger.warning(f'Predict queue full, skipping frame @ ts: {now}')
+            time.sleep(self.fps / 1000)
+
+    def _consumer(self):
+        '''
+            Calculates prediction and publishes result to subscriber queues
+        '''
+        logger.info('Started PredictWorker.consumer thread')
+
+        while True:
+            
+            msg = self._task_queue.get(block=True)
+
+            msg['image_buffer'].name = "original_image.jpg"
+            image = self._predictor.load_image(msg['image_buffer'])
+            prediction = self._predictor.predict(image)
+
+            viz_np = self._predictor.postprocess(
+                image, prediction
+            )
+            viz_image = Image.fromarray(viz_np, "RGB")
+            viz_buffer = io.BytesIO()
+            viz_buffer.name = "annotated_image.jpg"
+            viz_image.save(viz_buffer, format="JPEG")
+            viz_bytes = viz_buffer.getvalue()
+            
+            self._web_queue.put(viz_bytes)
+            self._websocket_queue.put({
+                'original_image': original_image,
+                'annotated_image': viz_buffer,
+                'prediction': prediction
+            })
+            self._task_queue.done()
+
+
