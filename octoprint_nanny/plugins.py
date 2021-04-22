444e8a8c7df4da15dd96b360bd2094fd3e2ae81d
Leigh Johnson <leigh@bitsy.ai>
2021-04-22 03:56:56 +0100

    rm an unused error/warning message

--- octoprint_nanny/plugins.py
+++ octoprint_nanny/plugins.py
@@ -243,6 +243,7 @@ class OctoPrintNannyPlugin(
         self._settings.set(["device_cloudiot_id"], None)
         self._settings.set(["device_registered"], False)
         self._settings.save()
+        self._worker_manager.reset_device_settings_state()
 
     @beeline.traced("OctoPrintNannyPlugin.sync_printer_profiles")
     async def sync_printer_profiles(self, **kwargs):
