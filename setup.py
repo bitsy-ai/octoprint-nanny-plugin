912d6e663afb010a813f8f44826a343918cd066c
Leigh Johnson <leigh@bitsy.ai>
2021-06-06 05:35:39 +0100

    Bump version: 0.6.5 → 0.6.6rc1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ plugin_package = "octoprint_nanny"
 plugin_name = "OctoPrint Nanny"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.6.5"
+plugin_version = "0.6.6rc1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
