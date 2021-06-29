425cebcc84a4d7d00cb3242a7a5a147aa6a869b1
Leigh Johnson <leigh@bitsy.ai>
2021-06-29 22:23:45 +0100

    Bump version: 0.7.1 → 0.7.2rc2

--- octoprint_nanny/__init__.py
+++ octoprint_nanny/__init__.py
@@ -7,7 +7,7 @@ __plugin_name__ = "OctoPrint Nanny"
 
 __plugin_pythoncompat__ = ">=3,<4"  # only python 3
 
-__plugin_version__ = "0.7.1"
+__plugin_version__ = "0.7.2rc2"
 
 
 def __plugin_load__():
