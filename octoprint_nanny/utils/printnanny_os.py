28f191f4cb86624d13ac9bf0432d8e7805c519b3
Leigh Johnson <leigh@bitsy.ai>
2022-02-05 21:43:42 -0800

    fix mypy erros

--- octoprint_nanny/utils/printnanny_os.py
+++ octoprint_nanny/utils/printnanny_os.py
@@ -1,5 +1,6 @@
 from typing import Optional, Any, Dict
 import logging
+import json
 import subprocess
 import socket
 
@@ -8,32 +9,39 @@ logger = logging.getLogger("octoprint.plugins.octoprint_nanny.utils")
 def printnanny_cli_version() -> Optional[str]:
     cmd = ["printnanny", "--version"]
     p = subprocess.run(cmd, capture_output=True)
+    stdout = p.stdout.decode('utf-8')
+    stderr = p.stderr.decode('utf-8')
     if p.returncode != 0:
-        logger.warning(f"Failed to get printnanny_cli_version cmd={cmd} returncode={p.returncode} stdout={p.stdout} stderr={p.stderr}")
-        return
-    logger.info(f"Found printnanny_cli_version={p.stdout}")
-    return p.stdout
+        logger.warning(f"Failed to get printnanny_cli_version cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}")
+        return None
+    logger.info(f"Found printnanny_cli_version={stdout}")
+    return stdout
 
 def printnanny_image_version() -> Optional[str]:
     cmd = ["cat", "/boot/image_version.txt"]
     p = subprocess.run(cmd, capture_output=True)
+    stdout = p.stdout.decode('utf-8')
+    stderr = p.stderr.decode('utf-8')
     if p.returncode != 0:
-        logger.warning(f"Failed to get printnanny_image_version cmd={cmd} returncode={p.returncode} stdout={p.stdout} stderr={p.stderr}")
-        return
-    logger.info(f"Found printnanny_image_version={p.stdout}")
-    return p.stdout
+        logger.warning(f"Failed to get printnanny_image_version cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}")
+        return None
+    logger.info(f"Found printnanny_image_version={stdout}")
+    return stdout
 
 def printnanny_config() -> Optional[Dict[str, Any]]:
     hostname = socket.gethostname()
     cmd = ["printnanny", "config", "get"]
     p = subprocess.run(cmd, capture_output=True)
+    stdout = p.stdout.decode('utf-8')
+    stderr = p.stderr.decode('utf-8')
     if p.returncode != 0:
-        logger.warning(f"Failed to get printnanny config cmd={cmd} returncode={p.returncode} stdout={p.stdout} stderr={p.stderr}")
-        return
+        logger.warning(f"Failed to get printnanny config cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}")
+        return None
     try:
-        logger.info()
-        return json.loads(cmd.stdout)
+        config = json.loads(stdout)
+        logger.info(f"Read printnanny config: {config}")
+        return config
     except json.JSONDecodeError as e:
         logger.error(e)
-        logger.error(f"Failed to decode printnanny config: {cmd.stdout}")
-        return
+        logger.error(f"Failed to decode printnanny config: {stdout}")
+        return None
---
+++ tests/utils/test_printnanny_os.py
@@ -0,0 +1,5 @@
+from octoprint_nanny.utils import (
+    printnanny_config,
+    printnanny_image_version,
+    printnanny_cli_version
+)
