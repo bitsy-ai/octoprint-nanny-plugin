46d1dfabf99aeb9cbd9a01ea39f5bb419c6add59
Leigh Johnson <leigh@bitsy.ai>
2022-06-21 16:56:11 -0700

    fix events tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -4,18 +4,18 @@ from unittest.mock import patch
 from octoprint_nanny.events import should_publish_event, try_handle_event
 
 
-@patch("octoprint_nanny.events.try_write_socket")
-def test_handle_untracked_event(mock_try_write_socket):
+@patch("octoprint_nanny.events.try_publish_cmd")
+def test_handle_untracked_event(mock_try_publish_cmd):
     try_handle_event(
         "someuntrackedevent",
         dict(),
     )
-    assert not mock_try_write_socket.called
+    assert mock_try_publish_cmd.called is False
 
 
 @patch("octoprint_nanny.events.load_printnanny_config")
-@patch("octoprint_nanny.events.try_write_socket")
-def test_handle_events_enabled_true(mock_try_write_socket, mock_printnanny_config):
+@patch("octoprint_nanny.events.try_publish_cmd")
+def test_handle_events_enabled_true(mock_try_publish_cmd, mock_printnanny_config):
     mock_printnanny_config.return_value = PrintNannyConfig(
         cmd=["mock", "cmd"],
         stdout="",
@@ -24,24 +24,29 @@ def test_handle_events_enabled_true(mock_try_write_socket, mock_printnanny_confi
         config={
             "device": {"id": 1},
             "paths": {"events_socket": "test.sock"},
-            "octoprint_server": {"id": 1},
+            "octoprint": {"server": {"id": 1}},
         },
     )
     try_handle_event(
         "Startup",
         dict(),
     )
-    assert mock_try_write_socket.called is True
+    assert mock_try_publish_cmd.called is True
 
 
-@patch("octoprint_nanny.events.load_printnanny_config")
+@patch("octoprint_nanny.utils.printnanny_os.load_printnanny_config")
 def test_should_publish_print_progress(mock_printnanny_config):
     mock_printnanny_config.return_value = PrintNannyConfig(
         cmd=["mock", "cmd"],
         stdout="",
         stderr="",
         returncode=0,
-        config={"alert_settings": {"print_progress_percent": 25}},
+        config={
+            "alert_settings": {
+                "print_progress_percent": 25,
+            },
+            "octoprint": {"server": {"id": 1}},
+        },
     )
     event = "PrintProgress"
     payload = dict(
