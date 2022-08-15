79863cbba28687ea58bae82083fe1b914da34081
Leigh Johnson <leigh@bitsy.ai>
2022-08-15 14:29:56 -0700

    bump printnanny-api-client to 0.101.1

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ plugin_requires = [
     "pytz",
     "aiohttp[speedups]>=3.7.4",
     # beta api client supporting PrintNanny OS in 2022
-    "printnanny-api-client~=0.100.9",
+    "printnanny-api-client~=0.101.1",
     "backoff>=1.10.0",
 ]
 
