4132ef592a0c918adc6c5909a849e2729408ba6e
Leigh Johnson <leigh@bitsy.ai>
2023-03-22 13:46:08 -0700

    install printnanny-octoprint-models

--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ plugin_requires = [
     "typing_extensions ; python_version < '3.8'",
     "pytz",
     "aiohttp[speedups]>=3.7.4",
-    "printnanny-api-client>=0.127.4",
+    "printnanny-api-client>=0.129.0",
+    "printnanny-octoprint-models>=0.1.0",
     "backoff>=1.10.0",
     "nats-py[nkeys]==2.2.0",
 ]
