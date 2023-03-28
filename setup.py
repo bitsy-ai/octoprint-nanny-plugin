abc5b4bc672fed9e17399468749f4dc978d11322
Leigh Johnson <leigh@bitsy.ai>
2023-03-27 20:45:04 -0700

    bump printnanny-api-client to 0.131.2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ plugin_requires = [
     "typing_extensions ; python_version < '3.8'",
     "pytz",
     "aiohttp[speedups]>=3.7.4",
-    "printnanny-api-client>=0.129.0",
+    "printnanny-api-client>=0.131.2",
     "printnanny-octoprint-models>=0.1.4",
     "backoff>=1.10.0",
     "nats-py[nkeys]==2.2.0",
